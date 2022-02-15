89c748f62b27b35d646b125239ee75968f0223ef
TMysiv <tarasmysiv@gmail.com>
2022-02-15 19:05:22 +0200

    add homework

--- Task1/Homework/app.js
+++ Task1/Homework/app.js
@@ -19,55 +19,55 @@ fs.mkdir(path.join(__dirname, 'main'), (err) => {
         throw err
 
     }
-    fs.mkdir(path.join(__dirname, 'main', 'online'), (err) => {
-        if (err) {
-            console.log(err)
-            throw err
-        }
-    })
-    fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
-        if (err) {
-            console.log(err)
-            throw err
-        }
-    })
-    for (let i = 0; i < onlineUsers.length; i++) {
-        fs.writeFile(path.join(__dirname, 'main', 'online', 'file1.txt'),
-            `\n name:${onlineUsers[i].name}, age:${onlineUsers[i].age}, city:${onlineUsers[i].city}`,
-            {flag: 'a'}, (err) => {
-                if (err) {
-                    console.log(err)
-                    throw err
-                }
-            })
-    }
-
-    for (let i = 0; i < inPersonUsers.length; i++) {
-        fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'file1.txt'),
-            `\n name:${inPersonUsers[i].name}, age:${inPersonUsers[i].age}, city:${inPersonUsers[i].city}`,
-            {flag: 'a'}, (err) => {
-                if (err) {
-                    console.log(err)
-                    throw err
-                }
-            })
+})
+fs.mkdir(path.join(__dirname, 'main', 'online'), (err) => {
+    if (err) {
+        console.log(err)
+        throw err
     }
 })
-
-
-const changeUsers = (prev,next) => {
-    fs.readFile(path.join(__dirname,'main',`${prev}`,'file1.txt'),(err,data)=>{
-        if (err){
-            console.log(err)
-            throw err
-        }
-        fs.appendFile(path.join(__dirname,'main',`${next}`,'file1.txt'),`${data.toString()}`,
-            {flag:'w'}, (err)=>{
-            if (err){
+fs.mkdir(path.join(__dirname, 'main', 'inPerson'), (err) => {
+    if (err) {
+        console.log(err)
+        throw err
+    }
+})
+for (let i = 0; i < onlineUsers.length; i++) {
+    fs.writeFile(path.join(__dirname, 'main', 'online', 'file1.txt'),
+        `\n name:${onlineUsers[i].name}, age:${onlineUsers[i].age}, city:${onlineUsers[i].city}`,
+        {flag: 'a'}, (err) => {
+            if (err) {
                 console.log(err)
                 throw err
             }
         })
+}
+
+for (let i = 0; i < inPersonUsers.length; i++) {
+    fs.writeFile(path.join(__dirname, 'main', 'inPerson', 'file1.txt'),
+        `\n name:${inPersonUsers[i].name}, age:${inPersonUsers[i].age}, city:${inPersonUsers[i].city}`,
+        {flag: 'a'}, (err) => {
+            if (err) {
+                console.log(err)
+                throw err
+            }
+        })
+}
+
+
+const changeUsers = (prev, next) => {
+    fs.readFile(path.join(__dirname, 'main', `${prev}`, 'file1.txt'), (err, data) => {
+        if (err) {
+            console.log(err)
+            throw err
+        }
+        fs.appendFile(path.join(__dirname, 'main', `${next}`, 'file1.txt'), `${data.toString()}`,
+            {flag: 'w'}, (err) => {
+                if (err) {
+                    console.log(err)
+                    throw err
+                }
+            })
     })
 }
 
