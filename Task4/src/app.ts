efc95018c76f47a284a30e9c240738ec433a8565
TMysiv <tarasmysiv@gmail.com>
2022-02-22 22:48:38 +0200

    add task4

--- Task4/src/app.ts
+++ Task4/src/app.ts
@@ -2,6 +2,6 @@ import express from 'express';
 
 const app = express();
 
-app.listen(5200,()=>{
-    console.log('Server has started')
-})
+app.listen(5300, () => {
+    console.log('Server has started!!!!!!!!');
+});
