6279769286a59f51b7a882de9080b7a706a9de94
TMysiv <tarasmysiv@gmail.com>
2022-04-08 19:02:22 +0300

    add socket

--- Task11/src/app.ts
+++ Task11/src/app.ts
@@ -1,12 +1,29 @@
 import express from 'express';
+import http from 'http';
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import SocketIO from 'socket.io';
 
 import { apiRouter } from './routes/apiRouter';
 import { config } from './config/config';
 import { cronRun } from './cron';
 
 const app = express();
+const server = http.createServer(app);
+
+// @ts-ignore
+const io = SocketIO(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket:any) => {
+    socket.on('join_room', (data:any) => {
+        socket.join(data.id);
+        socket.broadcast.to(data.id).emit('user_join_room', { message: `User ${data.id} joined room` });
+    });
+
+    socket.on('send message', (data:string) => {
+        io.emit('new message', data);
+    });
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,7 +33,7 @@ app.use(apiRouter);
 // @ts-ignore
 global.rootDir = __dirname;
 
-app.listen(config.PORT, async () => {
+server.listen(config.PORT, async () => {
     console.log(`Server has started!!!!!! on PORT:${config.PORT}`);
     try {
         const connection = await createConnection();
